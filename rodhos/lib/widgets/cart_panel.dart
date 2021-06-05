2edac906717f4d8aec027bd5d9ae33123c6a9246
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-06-05 18:34:43 -0400

    completed cart screen

--- rodhos/lib/widgets/cart_panel.dart
+++ rodhos/lib/widgets/cart_panel.dart
@@ -20,60 +20,61 @@ class _CartPanelState extends State<CartPanel> {
     var subTotal = (widget.orderItem.dish.price * widget.orderItem.quantity)
         .toStringAsFixed(2);
     return Container(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              widget.orderItem.dish.title,
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 4,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            widget.orderItem.dish.title,
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  order.decreaseCount(widget.orderItem.orderItemId);
+                },
+                icon: Icon(Icons.remove_circle_outline_rounded),
+                color: Colors.red,
+              ),
+              Container(
+                child: Center(
+                  child: Text(
+                    widget.orderItem.quantity.toString(),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  order.increaseCount(widget.orderItem.orderItemId);
+                },
+                icon: Icon(
+                  Icons.add_circle_outline_rounded,
+                  color: Colors.green,
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: mQuery.size.width * 0.175,
+            child: Text(
+              '\$' + subTotal,
               style: TextStyle(
-                fontSize: 18,
+                fontSize: 17,
               ),
+              softWrap: true,
+              textAlign: TextAlign.right,
             ),
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    order.decreaseCount(widget.orderItem.orderItemId);
-                  },
-                  icon: Icon(Icons.remove_circle_outline_rounded),
-                  color: Colors.red,
-                ),
-                Container(
-                  child: Center(
-                    child: Text(
-                      widget.orderItem.quantity.toString(),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    order.increaseCount(widget.orderItem.orderItemId);
-                  },
-                  icon: Icon(
-                    Icons.add_circle_outline_rounded,
-                    color: Colors.green,
-                  ),
-                )
-              ],
-            ),
-            Container(
-              width: mQuery.size.width * 0.175,
-              child: Text(
-                '\$' + subTotal,
-                style: TextStyle(
-                  fontSize: 17,
-                ),
-                softWrap: true,
-                textAlign: TextAlign.right,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
       height: mQuery.size.height * 0.085,
-      margin: EdgeInsets.symmetric(horizontal: 3),
+      margin: EdgeInsets.symmetric(horizontal: 8),
       decoration: BoxDecoration(
         border: Border(
           bottom: BorderSide(
