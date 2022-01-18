b40aa0643b9e7ea34420c6e2f2f0f19fe8cd6f30
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-01-18 13:09:04 -0500

    creating folder structure and empty files

--- smart_home/lib/core/app/app.dart
+++ smart_home/lib/core/app/app.dart
@@ -7,6 +7,10 @@ class SmartHomeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       home: Container(),
     );
   }
