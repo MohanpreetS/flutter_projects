e47255f12ddb2bba0d0f04f6925654fefab0fa66
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-01-24 13:09:04 -0500

    text theme added

---
+++ smart_home/lib/core/theme/sh_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'sh_colors.dart';
+
+abstract class SHTheme {
+  static ThemeData dark = ThemeData(
+      textTheme: TextTheme(
+    bodyLarge: GoogleFonts.gruppo(
+      fontSize: 16,
+      fontWeight: FontWeight.w800,
+      color: SHColors.textColor,
+    ),
+    bodyMedium: GoogleFonts.gruppo(
+      fontSize: 14,
+      fontWeight: FontWeight.w800,
+      color: SHColors.textColor,
+    ),
+    bodySmall: GoogleFonts.montserrat(
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      color: SHColors.textColor,
+    ),
+    displayLarge: GoogleFonts.buda(
+      fontSize: 70,
+      color: SHColors.textColor,
+    ),
+    displayMedium: GoogleFonts.buda(
+      fontSize: 50,
+      color: SHColors.textColor,
+    ),
+    displaySmall: GoogleFonts.buda(
+      fontSize: 40,
+      color: SHColors.textColor,
+    ),
+  ));
+}
