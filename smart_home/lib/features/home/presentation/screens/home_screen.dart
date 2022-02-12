328bc8011df7fd0749f8448e07b9baf911e3cc9e
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-02-12 13:09:04 -0500

    Smart rooms page view widget

--- smart_home/lib/features/home/presentation/screens/home_screen.dart
+++ smart_home/lib/features/home/presentation/screens/home_screen.dart
@@ -53,11 +53,62 @@ class _HomeScreenState extends State<HomeScreen> {
               Expanded(
                 child: Stack(
                   fit: StackFit.expand,
-                  children: [],
+                  children: [
+                    SmartRoomsPageView(
+                      pageNotifier: pageNotifier,
+                      roomSelectorNotifier: roomSelectorNotifier,
+                      controller: controller,
+                    )
+                  ],
                 ),
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SmartRoomsPageView extends StatelessWidget {
+  const SmartRoomsPageView({
+    super.key,
+    required this.pageNotifier,
+    required this.roomSelectorNotifier,
+    required this.controller,
+  });
+
+  final ValueNotifier<double> pageNotifier;
+  final ValueNotifier<int> roomSelectorNotifier;
+  final PageController controller;
+
+  double _getOffsetX(double percent) => percent.isNegative ? 30.0 : -30.0;
+
+  Matrix4 _getOutTranslate(double percent, int selected, int index) {
+    final x = selected != index && selected != -1 ? _getOffsetX(percent) : 0.0;
+    return Matrix4.translationValues(x, 0, 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<double>(
+      valueListenable: pageNotifier,
+      builder: (_, page, __) => ValueListenableBuilder(
+        valueListenable: roomSelectorNotifier,
+        builder: (_, selected, __) => PageView.builder(
+          controller: controller,
+          itemBuilder: (_, index) {
+            final percent = page - index;
+            final isSelected = selected == index;
+            final room = SmartRoom.fakeValues[index];
+            return AnimatedContainer(
+              duration: kThemeAnimationDuration,
+              curve: Curves.fastOutSlowIn,
+              transform: _getOutTranslate(percent, selected, index),
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              // child:
+            );
+          },
         ),
       ),
     );
