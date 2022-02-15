cf39abc2cf67b175008f4577fdd541b403e79ced
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-02-15 13:09:04 -0500

    device icon switcher

--- smart_home/lib/features/home/presentation/widgets/background_room_lights.dart
+++ smart_home/lib/features/home/presentation/widgets/background_room_lights.dart
@@ -4,6 +4,48 @@ import 'package:ui_common/ui_common.dart';
 
 import '../../../../core/core.dart';
 
+class _DeviceIconSwitcher extends StatelessWidget {
+  const _DeviceIconSwitcher({
+    required this.onTap,
+    required this.label,
+    required this.icon,
+    required this.value,
+  });
+
+  final Text label;
+  final Icon icon;
+  final bool value;
+  final ValueChanged<bool> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = value ? SHColors.selectedColor : SHColors.textColor;
+    return InkWell(
+      onTap: () => onTap(!value),
+      child: Column(
+        children: [
+          IconTheme(
+            data: IconThemeData(color: color, size: 24.sp),
+            child: icon,
+          ),
+          const SizedBox(height: 4),
+          DefaultTextStyle(
+            style: context.bodySmall.copyWith(color: color),
+            child: label,
+          ),
+          Text(
+            value ? 'ON' : 'OFF',
+            style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w700,
+              color: color,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class _RoomInfoRow extends StatelessWidget {
   const _RoomInfoRow({
     required this.icon,
