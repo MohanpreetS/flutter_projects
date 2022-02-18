7af03d8319f2a6c0e798a536e6d3c8e3fc5415d0
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-02-18 13:09:04 -0500

    border dot widget

--- smart_home/lib/features/home/presentation/widgets/page_indicators.dart
+++ smart_home/lib/features/home/presentation/widgets/page_indicators.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 import '../../../../core/shared/domain/entities/smart_room.dart';
 import '../../../../core/theme/sh_colors.dart';
 
+class _BorderDot extends StatelessWidget {
+  const _BorderDot();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 12,
+      height: 12,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.orange, width: 2),
+          color: SHColors.backgroundColor,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
+
 class _Dot extends StatelessWidget {
   const _Dot();
 
