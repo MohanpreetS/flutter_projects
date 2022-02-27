362fc3e0baa4f841caf37acadd627d28859cf8b3
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-02-27 13:09:04 -0500

    air switcher widget

---
+++ smart_home/lib/features/smart_room/widgets/air_conditiioner_controls_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../core/core.dart';
+
+class _AirSwitcher extends StatelessWidget {
+  const _AirSwitcher({
+    required this.room,
+  });
+
+  final SmartRoom room;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('Air conditioning'),
+        const SizedBox(height: 12),
+        Row(
+          children: [
+            Expanded(
+              child: SHSwitcher(
+                icon: const Icon(SHIcons.fan),
+                value: room.airCondition.isOn,
+                onChanged: (value) {},
+              ),
+            ),
+            const Spacer(),
+            Text(
+              '${room.airCondition.value}˚',
+              style: const TextStyle(fontSize: 28),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
