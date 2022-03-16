70d2e15016d0b9d3043f803cb1b7b217ebc4ccaf
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-03-16 13:09:04 -0400

    added sign up text

--- animated_login/lib/auth_screen.dart
+++ animated_login/lib/auth_screen.dart
@@ -135,6 +135,38 @@ class _AuthScreenState extends State<AuthScreen> with SingleTickerProviderStateM
                   ),
                 ),
               ),
+              //Sign up
+              AnimatedPositioned(
+                duration: defaultDuration,
+                bottom: !_isShowSignUp ? _size.height / 2 - 80 : _size.height * 0.3,
+                right: _isShowSignUp ? _size.width * 0.44 - 80 : 0,
+                child: DefaultTextStyle(
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: !_isShowSignUp ? 20 : 32,
+                    fontWeight: FontWeight.bold,
+                    color: !_isShowSignUp ? Colors.white : Colors.white70,
+                  ),
+                  child: Transform.rotate(
+                    angle: (90 - _animationTextRotate.value) * pi / 180,
+                    alignment: Alignment.topRight,
+                    child: InkWell(
+                      onTap: () {
+                        if (!_isShowSignUp) {
+                          updateView();
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: defpaultPadding * 0.75),
+                        width: 160,
+                        child: Text(
+                          "SIGN UP",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           );
         },
