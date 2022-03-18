f9fac8818464d4c16b689ade44c7231647a0ac8e
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-03-18 13:09:04 -0400

    car control app setup

---
+++ Car-Control-App-UI-main/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tesla Car App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      // home: HomeScreen(),
+    );
+  }
+}
