d80d79ed36747275ef1a37ecefce381629e3a262
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-03-24 13:09:04 -0400

    temp details widget

---
+++ Car-Control-App-UI-main/lib/screens/components/temp_details.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tesla_animated_app/constants.dart';
+import 'package:tesla_animated_app/home_controller.dart';
+import 'package:tesla_animated_app/screens/components/tmp_btn.dart';
+
+class TempDetails extends StatelessWidget {
+  const TempDetails({
+    Key? key,
+    required HomeController controller,
+  })  : _controller = controller,
+        super(key: key);
+
+  final HomeController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(defaultPadding),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 120,
+            child: Row(
+              children: [
+                TempBtn(
+                  isActive: _controller.isCoolSelected,
+                  svgSrc: "assets/icons/coolShape.svg",
+                  title: "COOL",
+                  press: _controller.updateCoolSelectedTab,
+                ),
+                const SizedBox(
+                  width: defaultPadding,
+                ),
+                TempBtn(
+                  isActive: !_controller.isCoolSelected,
+                  svgSrc: "assets/icons/heatShape.svg",
+                  title: "HEAT",
+                  activeColor: redColor,
+                  press: _controller.updateCoolSelectedTab,
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          Column(
+            children: [
+              IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: _controller.upCarTemp,
+                icon: Icon(
+                  Icons.arrow_drop_up,
+                  size: 48,
+                ),
+              ),
+              Text(
+                _controller.carTemp.toString() + "\u2103",
+                style: TextStyle(fontSize: 86),
+              ),
+              IconButton(
+                padding: EdgeInsets.zero,
+                onPressed: _controller.downCarTemp,
+                icon: Icon(
+                  Icons.arrow_drop_down,
+                  size: 48,
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Text("CURRENT TEMPERATURE"),
+          const SizedBox(height: defaultPadding),
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "INSIDE",
+                  ),
+                  Text(
+                    "20" + "\u2103",
+                    style: Theme.of(context).textTheme.headline5,
+                  )
+                ],
+              ),
+              const SizedBox(width: defaultPadding),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "OUTSIDE",
+                    style: TextStyle(color: Colors.white54),
+                  ),
+                  Text(
+                    "32" + "\u2103",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5!
+                        .copyWith(color: Colors.white54),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
