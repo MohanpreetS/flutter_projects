5afbab6fa6c8f71d6f2a9560055e76ee4f4a62b2
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-03-26 13:09:04 -0400

    tab change and door locks buttons

--- Car-Control-App-UI-main/lib/home_controller.dart
+++ Car-Control-App-UI-main/lib/home_controller.dart
@@ -7,4 +7,29 @@ class HomeController extends ChangeNotifier {
   bool isTrunkLock = true;
 
   int selectedBottomTab = 0;
+
+  void onBottomNavTabChange(int index) {
+    selectedBottomTab = index;
+    notifyListeners();
+  }
+
+  void updateRightDoorLock() {
+    isRightDoorLock = !isRightDoorLock;
+    notifyListeners();
+  }
+
+  void updateLeftDoorLock() {
+    isLeftDoorLock = !isLeftDoorLock;
+    notifyListeners();
+  }
+
+  void updateBonnetLock() {
+    isBonnetLock = !isBonnetLock;
+    notifyListeners();
+  }
+
+  void updateTrunkLock() {
+    isTrunkLock = !isTrunkLock;
+    notifyListeners();
+  }
 }
