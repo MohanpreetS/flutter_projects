2c4bef37ab55993a103ad6ba8dfe418e43a49250
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-03-30 13:09:04 -0400

    low pressure  psi text

--- Car-Control-App-UI-main/lib/screens/components/tyreCard.dart
+++ Car-Control-App-UI-main/lib/screens/components/tyreCard.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:tesla_animated_app/models/TyrePsi.dart';
 import '../../constants.dart';
 
+Widget lowPressureText(BuildContext context) {
+  return Column(
+    children: [
+      Text(
+        "LOW",
+        style: Theme.of(context).textTheme.headline3!.copyWith(color: Colors.white, fontWeight: FontWeight.w600),
+      ),
+      Text(
+        "PRESSURE",
+        style: TextStyle(fontSize: 20),
+      ),
+    ],
+  );
+}
+
 Text psiText(BuildContext context, {required String psi}) {
   return Text.rich(
     TextSpan(
