4c38169ac06236875aa74ba18a600aebbb7881d7
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-03-31 13:09:04 -0400

    tyre card widget

--- Car-Control-App-UI-main/lib/screens/components/tyreCard.dart
+++ Car-Control-App-UI-main/lib/screens/components/tyreCard.dart
@@ -2,35 +2,94 @@ import 'package:flutter/material.dart';
 import 'package:tesla_animated_app/models/TyrePsi.dart';
 import '../../constants.dart';
 
-Widget lowPressureText(BuildContext context) {
-  return Column(
-    children: [
-      Text(
-        "LOW",
-        style: Theme.of(context).textTheme.headline3!.copyWith(color: Colors.white, fontWeight: FontWeight.w600),
-      ),
-      Text(
-        "PRESSURE",
-        style: TextStyle(fontSize: 20),
-      ),
-    ],
-  );
-}
+class TyreCard extends StatelessWidget {
+  const TyreCard({
+    Key? key,
+    required this.isBottomTwoTyre,
+    required this.tyrePsi,
+  }) : super(key: key);
 
-Text psiText(BuildContext context, {required String psi}) {
-  return Text.rich(
-    TextSpan(
-      text: psi,
-      style: Theme.of(context).textTheme.headline4!.copyWith(
-            fontWeight: FontWeight.w600,
-            color: Colors.white,
-          ),
+  final bool isBottomTwoTyre;
+  final TyrePsi tyrePsi;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: tyrePsi.isLowPressure ? redColor.withOpacity(0.1) : Colors.white10,
+        border: Border.all(
+          color: tyrePsi.isLowPressure ? redColor : primaryColor,
+          width: 2,
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(6),
+        ),
+      ),
+      padding: EdgeInsets.all(defaultPadding),
+      child: isBottomTwoTyre
+          ? Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                lowPressureText(context),
+                const Spacer(),
+                psiText(context, psi: tyrePsi.psi.toString()),
+                const SizedBox(
+                  height: defaultPadding,
+                ),
+                Text(
+                  "${tyrePsi.temp}\u2103",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            )
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                psiText(context, psi: tyrePsi.psi.toString()),
+                const SizedBox(
+                  height: defaultPadding,
+                ),
+                Text(
+                  "${tyrePsi.temp}\u2103",
+                  style: TextStyle(fontSize: 16),
+                ),
+                const Spacer(),
+                lowPressureText(context),
+              ],
+            ),
+    );
+  }
+
+  Widget lowPressureText(BuildContext context) {
+    return Column(
       children: [
-        TextSpan(
-          text: "psi",
-          style: TextStyle(fontSize: 24),
+        Text(
+          "LOW",
+          style: Theme.of(context).textTheme.headline3!.copyWith(color: Colors.white, fontWeight: FontWeight.w600),
+        ),
+        Text(
+          "PRESSURE",
+          style: TextStyle(fontSize: 20),
         ),
       ],
-    ),
-  );
+    );
+  }
+
+  Text psiText(BuildContext context, {required String psi}) {
+    return Text.rich(
+      TextSpan(
+        text: psi,
+        style: Theme.of(context).textTheme.headline4!.copyWith(
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+        children: [
+          TextSpan(
+            text: "psi",
+            style: TextStyle(fontSize: 24),
+          ),
+        ],
+      ),
+    );
+  }
 }
