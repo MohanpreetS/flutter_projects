0e48eb59b43541d7fe57102dab897fdcdc51c592
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-04-30 13:09:04 -0400

    added tablet size

--- portfolio/lib/responsive.dart
+++ portfolio/lib/responsive.dart
@@ -20,6 +20,8 @@ class Responsive extends StatelessWidget {
 
   static bool isMobileLarge(BuildContext context) => MediaQuery.of(context).size.width <= 700;
 
+  static bool isTablet(BuildContext context) => MediaQuery.of(context).size.width < 1024;
+
   static bool isDesktop(BuildContext context) => MediaQuery.of(context).size.width >= 1024;
 
   @override
@@ -29,6 +31,8 @@ class Responsive extends StatelessWidget {
       return desktop;
     } else if (_size.width >= 700 && tablet != null) {
       return tablet!;
+    } else if (_size.width >= 450 && mobileLarge != null) {
+      return mobileLarge!;
     } else {
       return mobile;
     }
