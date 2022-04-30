2261f303a184d7374b23195ec7fabe8923a0cbad
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-04-30 13:09:04 -0400

    added responisive controller

---
+++ portfolio/lib/responsive.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+// We will modify it once we have our final design
+
+class Responsive extends StatelessWidget {
+  final Widget mobile;
+  final Widget? mobileLarge;
+  final Widget? tablet;
+  final Widget desktop;
+
+  const Responsive({
+    Key? key,
+    required this.mobile,
+    this.tablet,
+    required this.desktop,
+    this.mobileLarge,
+  }) : super(key: key);
+
+  static bool isMobile(BuildContext context) => MediaQuery.of(context).size.width <= 500;
+
+  static bool isMobileLarge(BuildContext context) => MediaQuery.of(context).size.width <= 700;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+    if (_size.width >= 1024) {
+      return desktop;
+    } else if (_size.width >= 700 && tablet != null) {
+      return tablet!;
+    }
+  }
+}
