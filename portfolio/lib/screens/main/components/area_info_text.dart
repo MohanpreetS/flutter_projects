6ec453b7bf41ad4838d92f90ffd10576811c0c62
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-05-05 13:09:04 -0400

    area info text

--- portfolio/lib/screens/main/components/area_info_text.dart
+++ portfolio/lib/screens/main/components/area_info_text.dart
@@ -14,7 +14,7 @@ class AreaInfoText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: defaultPadding / 2),
+      padding: const EdgeInsets.only(bottom: defaultPadding * 0.5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
