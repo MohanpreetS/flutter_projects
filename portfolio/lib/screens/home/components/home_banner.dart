dffb125a9274fd8d6883891e7a5bb5438d4fd72c
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-05-15 13:09:04 -0400

    animated text

--- portfolio/lib/screens/home/components/home_banner.dart
+++ portfolio/lib/screens/home/components/home_banner.dart
@@ -3,6 +3,45 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 
+class MyBuildAnimatedText extends StatelessWidget {
+  const MyBuildAnimatedText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      // it applies same style to all the widgets under it
+      style: Theme.of(context).textTheme.subtitle1!,
+      child: Row(
+        children: [
+          FlutterCodedText(),
+          SizedBox(width: defaultPadding / 2),
+          Text("I build "),
+          AnimatedTextKit(
+            animatedTexts: [
+              TyperAnimatedText(
+                "responsive web and mobile app.",
+                speed: Duration(milliseconds: 60),
+              ),
+              TyperAnimatedText(
+                "complete e-Commerce app UI.",
+                speed: Duration(milliseconds: 60),
+              ),
+              TyperAnimatedText(
+                "Chat app with dark and light theme.",
+                speed: Duration(milliseconds: 60),
+              ),
+            ],
+          ),
+          SizedBox(width: defaultPadding / 2),
+          FlutterCodedText(),
+        ],
+      ),
+    );
+  }
+}
+
 class FlutterCodedText extends StatelessWidget {
   const FlutterCodedText({
     Key? key,
