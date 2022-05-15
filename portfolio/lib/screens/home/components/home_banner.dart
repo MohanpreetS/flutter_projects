b28972e8e882164dd4989b81e64cf1f6f029849b
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-05-15 13:09:04 -0400

    home banner

--- portfolio/lib/screens/home/components/home_banner.dart
+++ portfolio/lib/screens/home/components/home_banner.dart
@@ -3,6 +3,58 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 
+class HomeBanner extends StatelessWidget {
+  const HomeBanner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 3,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            "assets/images/bg.jpeg",
+            fit: BoxFit.cover,
+          ),
+          Container(color: darkColor.withOpacity(0.66)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Discover my Amazing \nArt Space!",
+                  style: Theme.of(context).textTheme.headline3!.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                ),
+                MyBuildAnimatedText(),
+                SizedBox(height: defaultPadding),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: defaultPadding * 2, vertical: defaultPadding),
+                    backgroundColor: primaryColor,
+                  ),
+                  child: Text(
+                    "EXPLORE NOW",
+                    style: TextStyle(color: darkColor),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class MyBuildAnimatedText extends StatelessWidget {
   const MyBuildAnimatedText({
     Key? key,
