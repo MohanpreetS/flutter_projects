2b43e0c20d4bde4c3cfd3f236905885a345fa914
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2022-05-20 13:09:04 -0400

    added home screen widget

--- portfolio/lib/main.dart
+++ portfolio/lib/main.dart
@@ -20,9 +20,7 @@ class MyApp extends StatelessWidget {
         primaryColor: primaryColor,
         scaffoldBackgroundColor: bgColor,
         canvasColor: bgColor,
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-            .apply(bodyColor: Colors.white)
-            .copyWith(
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme).apply(bodyColor: Colors.white).copyWith(
               bodyText1: TextStyle(color: bodyTextColor),
               bodyText2: TextStyle(color: bodyTextColor),
             ),
